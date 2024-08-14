dcca2395a2e38ab6454a850d6f52584536fa2a01
sebtech974 <sebtech974@gmail.com>
2024-08-14 20:46:41 +0400

    Initial Commit

---
+++ FormationUdSwitUI/FormationUdSwitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormationUdSwitUIApp.swift
+//  FormationUdSwitUI
+//
+//  Created by SEBASTIEN BOUILLET on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FormationUdSwitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
